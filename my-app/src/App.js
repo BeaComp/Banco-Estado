016ec64a9f1c71f31c92f02cbe3a57f820899aba
PEDRO HENRIQUE DA ROCHA <rocha.2021@alunos.utfpr.edu.br>
2023-06-08 14:47:53 -0300

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
 import Login from './Pages/Login';
+import Home from './Pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <Home/>
     </div>
   );
 }
