ae3c39e9921a4950d04cfc0f081c6562ecd31d75
BeaComp <102777924+BeaComp@users.noreply.github.com>
2023-06-08 12:33:50 -0300

    Create Tela Login

---
+++ my-app/src/Pages/Login/index.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import './login.css';
+
+import logo from '../img/bancoestado-logo.png'
+import retangulo from '../img/Rectangle.png'
+
+function Login() {
+
+    return (
+        <div className="page-login">
+            
+            <div className="login">
+                <img src={logo} alt="Banco Estado" />
+                <p className="login-title">Entre com seus dados:</p>
+
+                <form className="form" autoComplete="none">
+                    <div className="form-control">
+                        <label htmlFor="numeroConta">Número da Conta:</label>
+                        <input id="numberConta" type="text" name="numberConta" autoComplete="none" placeholder="Ex:12345-5" />
+                    </div>
+
+                    <div className="form-control">
+                        <label htmlFor="password">Senha:</label>
+                        <input id="password" type="password" name="password" placeholder="*******" />
+                    </div>
+
+                    <button id="btnEntrar">Entrar</button>
+                </form>
+
+            </div>
+
+            <div className="quadrado">
+                
+            </div>
+        </div>
+
+    );
+};
+
+export default Login;
