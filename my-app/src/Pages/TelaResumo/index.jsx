70d01f1b8a25217029bef144b1060312f5927b3b
BeaComp <102777924+BeaComp@users.noreply.github.com>
2023-06-10 00:51:26 -0300

    Ajustes

--- my-app/src/Pages/TelaResumo/index.jsx
+++ my-app/src/Pages/TelaResumo/index.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import './TelaResumo.css';
 import icon from '../img/Icon.svg'
+import { useState } from 'react';
+import TelaMovimentos from "../TelaMovimentos";
+import TelaSaldoCorrente from "../TelaSaldo/ContaCorrente";
+
+
 
 
 const TelaResumo = () => {
 
     return (
+
         <div className="tela-resumo">
             <div className="resumo">
                 <h1>Resumo</h1>
@@ -33,16 +39,18 @@ const TelaResumo = () => {
             <div className="acesso">
                 <h1>Acesso Diretos</h1>
 
+            
+
                 <div className="acesso-itens">
                     <ul>
                         <li>
                             <img src={icon} alt="icon" />
-                            <p>Transferir</p>
+                            <p >Transferir</p>
                         </li>
 
                         <li>
                             <img src={icon} alt="icon" />
-                            <p>Consultar Saldo</p>
+                            <p> Consultar Saldo</p>
                         </li>
 
                         <li>
