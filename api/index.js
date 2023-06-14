9b00bf8c8de7a981979a4b3a2b8b8fe43626484b
PEDRO HENRIQUE DA ROCHA <rocha.2021@alunos.utfpr.edu.br>
2023-06-14 20:31:24 -0300

    teste de conexão com banco de dados

---
+++ api/index.js
@@ -0,0 +1,45 @@
+const { Pool } = require('pg');
+
+// Configuração da conexão com o banco de dados
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'Banco_Estado',
+  password: '123456',
+  port: 5432, // ou a porta que você configurou
+});
+
+// Verifica se a conexão com o banco de dados foi estabelecida
+pool.connect((err) => {
+  if (err) {
+    console.error('Erro ao conectar ao banco de dados', err);
+  } else {
+    console.log('Conexão com o banco de dados estabelecida');
+  }
+});
+
+const express = require('express');
+const app = express();
+const port = 3000; // Porta que esta rodando o server
+
+app.use(express.json());
+
+// Rota de exemplo
+app.get('/', (req, res) => {
+  res.send('API em execução!');
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
+
+app.get('/usuarios', async (req, res) => {
+    try {
+      const { rows } = await pool.query('select * from contas');
+      res.json(rows);
+      res.send('to na consulta')
+    } catch (error) {
+      console.error('Erro ao executar a consulta', error);
+      res.status(500).json({ error: 'Erro ao executar a consulta' });
+    }
+  });
